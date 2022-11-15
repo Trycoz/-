dc88f4f58a41cd59c00703479128b11be39c0020
Trycoz <101463191+Trycoz@users.noreply.github.com>
2022-11-15 20:02:53 -0300

    Noaceptaletrapog

--- converter.js
+++ converter.js
@@ -2,6 +2,7 @@
 function convertUnit(value) {
     let results = {}
     const unitNumber = Number(value)
+    const intBool = Number.isInteger(unitNumber)
     results.meterToFeet = (unitNumber * 3.280839895).toFixed(3)
     results.feetToMeter = (unitNumber * 0.3048).toFixed(3)
     results.literToGallon = (unitNumber * 0.2641720524).toFixed(3)
@@ -9,28 +10,35 @@ function convertUnit(value) {
     results.kilosToPounds = (unitNumber * 2.2046226218).toFixed(3)
     results.poundsToKilos = (unitNumber * 0.45359237).toFixed(3)
 
-    return results
+    return [results, intBool]
 }
 
 function showUnits() {
 
-    const inputValue = document.getElementById("unitToConvert").value
-    resultsObject = convertUnit(inputValue)
-    document.getElementById("cardsConvertions").innerHTML =
+    let inputValue = document.getElementById("unitToConvert").value
+    const resultsObject = convertUnit(inputValue)
+    console.log(resultsObject)
+    if (resultsObject[1]) {
+
+        document.getElementById("cardsConvertions").innerHTML =
+            `
+            <div class="cardContent">
+              <h5>Length (Meter/Feet)</h5>
+              <h6>${inputValue} meters = ${resultsObject[0].meterToFeet} feet | ${inputValue} feet = ${resultsObject[0].feetToMeter} meters</h6>
+            </div>
+            <div class="cardContent">
+              <h5>Volume (Liters/Gallons)</h5>
+              <h6>${inputValue} liters = ${resultsObject[0].literToGallon} gallons | ${inputValue} gallons = ${resultsObject[0].gallonToLiter} liters</h6>
+            </div>
+            <div class="cardContent">
+              <h5>Mass (Kilograms/Pounds)</h5>
+              <h6>${inputValue} kilos = ${resultsObject[0].kilosToPounds} pounds | ${inputValue} pounds = ${resultsObject[0].poundsToKilos} kilos</h6>
+            </div>
         `
-        <div class="cardContent">
-          <h5>Length (Meter/Feet)</h5>
-          <h6>${inputValue} meters = ${resultsObject.meterToFeet} feet | ${inputValue} feet = ${resultsObject.feetToMeter} meters</h6>
-        </div>
-        <div class="cardContent">
-          <h5>Volume (Liters/Gallons)</h5>
-          <h6>${inputValue} liters = ${resultsObject.literToGallon} gallons | ${inputValue} gallons = ${resultsObject.gallonToLiter} liters</h6>
-        </div>
-        <div class="cardContent">
-          <h5>Mass (Kilograms/Pounds)</h5>
-          <h6>${inputValue} kilos = ${resultsObject.kilosToPounds} pounds | ${inputValue} pounds = ${resultsObject.poundsToKilos} kilos</h6>
-        </div>
-    `
+    } else {
+        document.getElementById("unitToConvert").value = 0
+        showUnits()
+    }
 
 }
 
